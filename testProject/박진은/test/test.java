01bf8cea8f51f575c8d8ece47e3ee2acd0731c4a
JinEunPark <wlsdmsrnfl@naver.com>
2022-02-06 19:16:07 +0900

    being tested to modify2

--- testProject/박진은/test/test.java
+++ testProject/박진은/test/test.java
@@ -3,5 +3,8 @@ package test;
 public class test {
     public static void main(String[] args) {
         System.out.println("qkrwlsdms");
+        for(int i = 0; i< 100; i++){
+            System.out.println("qkrwlsdms");
+        }
     }
 }
