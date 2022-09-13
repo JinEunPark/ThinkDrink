6617c5722c13d5b8da83388166b0381bd95e0af1
parkjineun <hjkasbnm@gmail.com>
2022-09-13 09:12:05 +0900

    last up date

---
+++ src/main/java/com/datacampus/biohealth2/controller/ShowIntakeDrinkController.java
@@ -0,0 +1,39 @@
+package com.datacampus.biohealth2.controller;
+
+import com.datacampus.biohealth2.entity.BeverageIntake;
+import com.datacampus.biohealth2.repository.BeverageIntakeRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Getter
+@Setter
+@RequestMapping(value="members/showIntakeBeverage")
+public class ShowIntakeDrinkController {
+
+    private final BeverageIntakeRepository beverageIntakeRepository;
+
+    @GetMapping
+
+    public String getTimeStamp(){
+        return"Intake/showIntakeBeverage";
+    }
+
+    @GetMapping(value="/{timestamp}")
+    @ResponseBody
+    public List<BeverageIntake> getBeverageIntakeAsTimeStamp(String startTimeStamp, String endTimeStamp, String present){
+        return null;
+
+    }
+
+
+}
--- src/main/java/com/datacampus/biohealth2/repository/BeverageIntakeRepository.java
+++ src/main/java/com/datacampus/biohealth2/repository/BeverageIntakeRepository.java
@@ -3,6 +3,9 @@ package com.datacampus.biohealth2.repository;
 import com.datacampus.biohealth2.entity.BeverageIntake;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BeverageIntakeRepository extends JpaRepository<BeverageIntake,Long> {
+    List<BeverageIntake> findAllByRegTimeAfterAndRegTimeBefore(String startTimeStamp, String endTimeStamp);
 
 }
